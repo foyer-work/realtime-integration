c2d40fb84b8f1bc91350bc4fa3436a31309352a6
kunal00000 <kunalverma2468@gmail.com>
2024-10-14 01:38:08 +0530

    feat: connect to realtime, basic receive message to openai

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,15 @@ import { DurableObject } from 'cloudflare:workers';
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		if (request.url.endsWith('/websocket')) {
-			// Expect to receive a WebSocket Upgrade request.
-			// If there is one, accept the request and return a WebSocket Response.
 			const upgradeHeader = request.headers.get('Upgrade');
 			if (!upgradeHeader || upgradeHeader !== 'websocket') {
 				return new Response('Durable Object expected Upgrade: websocket', { status: 426 });
 			}
 
-			// This example will refer to the same Durable Object,
-			// since the name "foo" is hardcoded.
-			let id = env.MY_DURABLE_OBJECT.idFromName('foo');
-			let stub = env.MY_DURABLE_OBJECT.get(id);
-
-			return stub.fetch(request);
+			const accessToken = request.headers.get('Authorization');
+			const id = env.CONVERSATIONS.idFromName(`conversation-${accessToken}`);
+			const conversationDO = env.CONVERSATIONS.get(id);
+			return conversationDO.fetch(request);
 		}
 
 		return new Response(null, {
@@ -30,45 +26,100 @@ export default {
 } satisfies ExportedHandler<Env>;
 
 // Durable Object
-export class MyDurableObject extends DurableObject {
-	currentlyConnectedWebSockets: number;
+export class ConversationDO extends DurableObject {
+	env: Env;
+	messages: { role: string; content: string | ArrayBuffer }[];
+	openAISocket: WebSocket | null;
+	workerSocket: WebSocket | null;
 
 	constructor(ctx: DurableObjectState, env: Env) {
-		// This is reset whenever the constructor runs because
-		// regular WebSockets do not survive Durable Object resets.
-		//
-		// WebSockets accepted via the Hibernation API can survive
-		// a certain type of eviction, but we will not cover that here.
 		super(ctx, env);
-		this.currentlyConnectedWebSockets = 0;
+		this.env = env;
+		this.openAISocket = null;
+		this.workerSocket = null;
+		this.messages = [];
 	}
 
-	async fetch(request: Request): Promise<Response> {
-		// Creates two ends of a WebSocket connection.
+	async fetch(request: Request) {
 		const webSocketPair = new WebSocketPair();
 		const [client, server] = Object.values(webSocketPair);
 
-		// Calling `accept()` tells the runtime that this WebSocket is to begin terminating
-		// request within the Durable Object. It has the effect of "accepting" the connection,
-		// and allowing the WebSocket to send and receive messages.
-		server.accept();
-		this.currentlyConnectedWebSockets += 1;
-
-		// Upon receiving a message from the client, the server replies with the same message,
-		// and the total number of connections with the "[Durable Object]: " prefix
-		server.addEventListener('message', (event: MessageEvent) => {
-			server.send(`[Durable Object] currentlyConnectedWebSockets: ${this.currentlyConnectedWebSockets}\n message: ${event.data}`);
-		});
-
-		// If the client closes the connection, the runtime will close the connection too.
-		server.addEventListener('close', (cls: CloseEvent) => {
-			this.currentlyConnectedWebSockets -= 1;
-			server.close(cls.code, 'Durable Object is closing WebSocket');
-		});
+		await this.handleSession(server);
 
 		return new Response(null, {
 			status: 101,
 			webSocket: client,
 		});
+	}
+
+	async handleSession(workerWs: WebSocket) {
+		workerWs.accept();
+		this.workerSocket = workerWs;
+
+		this.openAISocket = await this.connectToOpenAI();
+
+		workerWs.addEventListener('message', async (event) => {
+			/**
+			 * Send the message to OpenAI
+			 * Store message in DurableObject state
+			 */
+
+			workerWs.send(`Sending "` + event.data + `" to OpenAI`);
+			if (this.openAISocket && this.openAISocket.readyState === WebSocket.OPEN) {
+				this.openAISocket.send(event.data);
+				this.storeMessage('user', event.data);
+			}
+		});
+
+		workerWs.addEventListener('close', async (cls: CloseEvent) => {
+			/**
+			 * Close the openAI socket
+			 * Store the conversation to the database (Arcane)
+			 * Close the workerWs socket
+			 */
+
+			if (this.openAISocket) {
+				this.openAISocket.close();
+			}
+			// await this.dumpConversationToArcane()
+
+			if (this.workerSocket) {
+				console.log('[ConversationDO] closing Worker WebSocket');
+				this.workerSocket = null;
+				workerWs.close(cls.code, `Worker Websocket is closing...`);
+			}
+		});
+	}
+
+	async connectToOpenAI() {
+		const url = 'wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01';
+		const openAISocket = new WebSocket(url, ['realtime', `openai-insecure-api-key.${this.env.OPENAI_API_KEY}`, 'openai-beta.realtime-v1']);
+
+		openAISocket.addEventListener('open', () => {
+			console.log('Connected to OpenAI server.');
+			openAISocket.send(
+				JSON.stringify({
+					type: 'response.create',
+					response: {
+						modalities: ['text'],
+						instructions: 'Hello!',
+					},
+				})
+			);
+		});
+
+		openAISocket.addEventListener('message', async (event) => {
+			if (this.workerSocket && this.workerSocket.readyState === WebSocket.OPEN) {
+				this.workerSocket.send(`Received "` + event.data + `" from OpenAI`);
+				this.storeMessage('assistant', event.data);
+			}
+		});
+
+		return openAISocket;
+	}
+
+	storeMessage(role: string, content: string | ArrayBuffer) {
+		this.messages.push({ role, content });
+		// await this.ctx.storage.put('messages', this.messages);
 	}
 }
