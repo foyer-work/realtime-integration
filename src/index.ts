7d34b56da43b2afaefccaa23b00c8614e064fd92
kunal00000 <kunalverma2468@gmail.com>
2024-10-15 00:39:56 +0530

    chore: relay working fine using realtimeclient

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,18 @@ import { DurableObject } from 'cloudflare:workers';
 // Worker
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		if (request.url.endsWith('/websocket')) {
+		const url = new URL(request.url);
+		if (url.pathname === '/websocket') {
 			const upgradeHeader = request.headers.get('Upgrade');
 			if (!upgradeHeader || upgradeHeader !== 'websocket') {
 				return new Response('Durable Object expected Upgrade: websocket', { status: 426 });
 			}
 
 			const accessToken = request.headers.get('Authorization');
+			console.log({ accessToken });
 			const id = env.CONVERSATIONS.idFromName(`conversation-${accessToken}`);
 			const conversationDO = env.CONVERSATIONS.get(id);
-			return conversationDO.fetch(request);
+			return conversationDO.fetch(request.clone());
 		}
 
 		return new Response(null, {
@@ -28,23 +30,23 @@ export default {
 // Durable Object
 export class ConversationDO extends DurableObject {
 	env: Env;
-	messages: { role: string; content: string | ArrayBuffer }[];
-	openAISocket: WebSocket | null;
+	messageQueue: (string | ArrayBuffer)[];
+	openAIClient: WebSocket | null;
 	workerSocket: WebSocket | null;
 
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
 		this.env = env;
-		this.openAISocket = null;
+		this.openAIClient = null;
 		this.workerSocket = null;
-		this.messages = [];
+		this.messageQueue = [];
 	}
 
-	async fetch(request: Request) {
+	fetch() {
 		const webSocketPair = new WebSocketPair();
 		const [client, server] = Object.values(webSocketPair);
 
-		await this.handleSession(server);
+		this.handleSession(server);
 
 		return new Response(null, {
 			status: 101,
@@ -52,74 +54,68 @@ export class ConversationDO extends DurableObject {
 		});
 	}
 
-	async handleSession(workerWs: WebSocket) {
+	handleSession(workerWs: WebSocket) {
 		workerWs.accept();
 		this.workerSocket = workerWs;
 
-		this.openAISocket = await this.connectToOpenAI();
+		this.openAIClient = this.connectToOpenAI();
 
-		workerWs.addEventListener('message', async (event) => {
+		workerWs.addEventListener('message', (event) => {
 			/**
 			 * Send the message to OpenAI
 			 * Store message in DurableObject state
 			 */
 
-			workerWs.send(`Sending "` + event.data + `" to OpenAI`);
-			if (this.openAISocket && this.openAISocket.readyState === WebSocket.OPEN) {
-				this.openAISocket.send(event.data);
-				this.storeMessage('user', event.data);
+			if (!this.openAIClient || this.openAIClient.readyState !== WebSocket.OPEN) {
+				this.messageQueue.push(event.data);
+			} else {
+				this.openAIClient.send(event.data);
 			}
 		});
 
-		workerWs.addEventListener('close', async (cls: CloseEvent) => {
+		workerWs.addEventListener('close', () => {
 			/**
 			 * Close the openAI socket
 			 * Store the conversation to the database (Arcane)
 			 * Close the workerWs socket
 			 */
 
-			if (this.openAISocket) {
-				this.openAISocket.close();
-			}
 			// await this.dumpConversationToArcane()
-
-			if (this.workerSocket) {
-				console.log('[ConversationDO] closing Worker WebSocket');
-				this.workerSocket = null;
-				workerWs.close(cls.code, `Worker Websocket is closing...`);
+			if (this.openAIClient && this.openAIClient.readyState === WebSocket.OPEN) {
+				console.log('[ConversationDO] Worker/Client closed the connection.');
+				this.openAIClient.close();
 			}
 		});
 	}
 
-	async connectToOpenAI() {
+	connectToOpenAI() {
 		const url = 'wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01';
-		const openAISocket = new WebSocket(url, ['realtime', `openai-insecure-api-key.${this.env.OPENAI_API_KEY}`, 'openai-beta.realtime-v1']);
+		const openAIClient = new WebSocket(url, ['realtime', `openai-insecure-api-key.${this.env.OPENAI_API_KEY}`, 'openai-beta.realtime-v1']);
 
-		openAISocket.addEventListener('open', () => {
+		openAIClient.addEventListener('open', () => {
 			console.log('Connected to OpenAI server.');
-			openAISocket.send(
-				JSON.stringify({
-					type: 'response.create',
-					response: {
-						modalities: ['text'],
-						instructions: 'Hello!',
-					},
-				})
-			);
-		});
 
-		openAISocket.addEventListener('message', async (event) => {
-			if (this.workerSocket && this.workerSocket.readyState === WebSocket.OPEN) {
-				this.workerSocket.send(`Received "` + event.data + `" from OpenAI`);
-				this.storeMessage('assistant', event.data);
+			while (this.messageQueue.length > 0) {
+				const message = this.messageQueue.shift();
+				if (message) {
+					openAIClient.send(message);
+				}
 			}
 		});
 
-		return openAISocket;
-	}
+		openAIClient.addEventListener('message', (event) => {
+			if (this.workerSocket && this.workerSocket.readyState === WebSocket.OPEN) {
+				this.workerSocket.send(event.data);
+			}
+		});
 
-	storeMessage(role: string, content: string | ArrayBuffer) {
-		this.messages.push({ role, content });
-		// await this.ctx.storage.put('messages', this.messages);
+		openAIClient.addEventListener('close', () => {
+			if (this.workerSocket && this.workerSocket.readyState === WebSocket.OPEN) {
+				console.log('OpenAI server closed the connection.');
+				this.workerSocket.close();
+			}
+		});
+
+		return openAIClient;
 	}
 }
